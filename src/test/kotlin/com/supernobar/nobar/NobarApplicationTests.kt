8802ad97a2790bc56fab573d45a53b1c28a26fdf
jinsu4755 <parkjinsu4755@gmail.com>
2022-11-27 20:34:54 +0900

    ci(check): test file 수정

--- src/test/kotlin/com/supernobar/nobar/NobarApplicationTests.kt
+++ src/test/kotlin/com/supernobar/nobar/NobarApplicationTests.kt
@@ -3,7 +3,7 @@ package com.supernobar.nobar
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(classes = [NobarApplicationTests::class])
 class NobarApplicationTests {
 
     @Test
